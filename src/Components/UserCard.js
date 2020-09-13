1265fdedef202751d19ddd5f249c1f0bd6d21322
Pradeep <pradeeprocks1999@gmail.com>
2020-09-13 12:01:35 +0530

    img tag value

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -4,7 +4,11 @@ import { Card, CardBody } from "reactstrap";
 export default function UserCard({ user }) {
   return (
     <Card className="text-center mt-3 mb-4">
-      <img src={user.avatar_url} className="img-thumbnail img-fluid" />
+      <img
+        src={user.avatar_url}
+        alt="your seach user image"
+        className="img-thumbnail img-fluid"
+      />
       <CardBody>
         <div className="text-primary">{user.name}</div>
         <div className="text-primary">{user.location}</div>
